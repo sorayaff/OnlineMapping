dac88fa88318f2b3e5ee9aa2eba148494fa43fb3
yehuaxin <512217680@qq.com>
2019-09-04 20:06:50 +0800

    图例样式更改，解决当图例移动到边缘时会触发滚动条的bug

--- src/components/Legend/ContinuousLegend.js
+++ src/components/Legend/ContinuousLegend.js
@@ -3,9 +3,10 @@ import styles from '@/components/Legend/index.less';
 import React, { useEffect, useState } from 'react';
 import { connect } from 'dva';
 
+
 function blobToDataURI(blob, callback) {
   let reader = new FileReader();
-  reader.onload = function (e) {
+  reader.onload = function(e) {
     callback(e.target.result);
   };
   reader.readAsDataURL(blob);
@@ -13,35 +14,39 @@ function blobToDataURI(blob, callback) {
 
 function ContinuousLegend(props) {
   const { dispatch } = props;
-  const [colorMapPic,setColorMapPic] = useState(null);
+  const [colorMapPic, setColorMapPic] = useState(null);  //色带图片
   const colorMapId = props.colorMapId;
   const colorbar = props.colorbar;
-  const containerHeight = props.containerHeight;
-  let tempMarginTop;
+  //传过来的高度值有可能是数字有可能是带'px'后缀的字符串，因此做一下处理
+  const containerHeight = parseInt(props.containerHeight.toString().replace('px', ''));
   const colorbarHeight = containerHeight - 20;
 
+  //拿到的是Blob格式的图片数据，需要转成DataURL后才可成为img标签的src
   const picBlob = props.currentColormapPic;
-  if(picBlob){
-    blobToDataURI(picBlob,function(data) {
+  if (picBlob) {
+    blobToDataURI(picBlob, function(data) {
       setColorMapPic(data);
-    })
+    });
   }
 
+  //色带样式
   const colorbarPicStyle = {
     width: containerHeight - 20,
     top: containerHeight - 10,
   };
 
+  //当colorMapId发生改变时请求色带图片
   useEffect(() => {
     dispatch({
       type: 'mapView/fetchColormapPicById',
       payload: {
-        colorMapId:colorMapId
+        colorMapId: colorMapId,
       },
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[colorMapId]);
+  }, [colorMapId]);
 
+  //色带图片的宽度会跟着容器高而改变，然后以左上角为起点旋转90度后往下压一定高度
   return (
     <div className={classNames(styles['legendContainer'])}>
       <div className={classNames(styles['card-for-pic'])}>
@@ -49,7 +54,6 @@ function ContinuousLegend(props) {
       </div>
       <div className={styles.textContainer}>
         {colorbar['legend'].map(item => {
-          tempMarginTop = (1 - item[0]) * 8 + 0.5 + 'em';
           return (<p className={classNames(styles['text'])} style={{ bottom: colorbarHeight * item[0] }}>{item[1]}</p>);
         })}
       </div>
@@ -58,6 +62,6 @@ function ContinuousLegend(props) {
 }
 
 export default connect(({ mapView }) => ({
-  currentColormapPic:mapView.currentColormapPic,
-  mapView
+  currentColormapPic: mapView.currentColormapPic,
+  mapView,
 }))(ContinuousLegend);
--- src/components/Legend/index.js
+++ src/components/Legend/index.js
@@ -1,28 +1,32 @@
 import React, { PureComponent, useState, useEffect } from 'react';
 import DiscreteLegend from './DiscreteLegend';
-import ContinuousLegend from './ContinuousLegend'
+import ContinuousLegend from './ContinuousLegend';
 import { connect } from 'dva';
 import { Rnd } from 'react-rnd';
 
 function Legend(props) {
-  const [width, setWidth] = useState(200);
-  const [height, setHeight] = useState(245);
-  const [x, setX] = useState(1200);
-  const [y, setY] = useState(450);
+  const [width, setWidth] = useState(200); //可拖动组件的宽
+  const [height, setHeight] = useState(245); //可拖动组建的高
+  const [x, setX] = useState(1200); //可拖动组件的x坐标
+  const [y, setY] = useState(450); //可拖动组建的y坐标
   const { dispatch } = props;
 
   const colorMapId = props.colorMapId || '34ac9adc-80b9-46c1-980a-716c0988bfe3';
 
-  const colorbar = props.currentColormap;
+  const colorbar = props.currentColormap; //从mapView模型中拿到当前的色带值
 
+  //可拖动组建的样式
   const style = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    border: 'solid 1px #ddd',
-    background: 'transparent',
+    borderRadius: 8,
+    boxShadow: '5px 10px 20px rgba(0,0,0,0.5)',
+    background: 'rgba(255,255,255,0.08',
+    margin: -20,
   };
 
+  //控制组件可以改变大小的方向
   const Enable = {
     bottom: false,
     bottomLeft: false,
@@ -35,23 +39,26 @@ function Legend(props) {
   };
 
 
-
-  useEffect(()=> {
+  //当colorMapId发生变更时发起获取色带数据的请求
+  useEffect(() => {
     dispatch({
       type: 'mapView/fetchColormapById',
       payload: {
-        colorMapId:colorMapId
+        colorMapId: colorMapId,
       },
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[colorMapId]);
+  }, [colorMapId]);
 
 
-  if(colorbar){
+  //如果根据colorMapId拿到了色带数据，则根据其中的type值判断应该返回离散图例还是连续渐变图例
+  //当需要返回连续渐变图例时会传递父组件的高度以便控制色带图片的样式
+  if (colorbar) {
+    const size = colorbar.type === 'discrete' ? null : { width: width, height: height };
     return (
       <Rnd
         style={style}
-        size={{ width: width, height: height }}
+        size={size}
         position={{ x: x, y: y }}
         onDragStop={(e, d) => {
           setX(d.x);
@@ -70,14 +77,13 @@ function Legend(props) {
           <ContinuousLegend colorbar={colorbar} containerHeight={height} colorMapId={colorMapId}/>}
       </Rnd>
     );
-  }
-  else{
-    return (<div/>)
+  } else {
+    return (<div/>);
   }
 }
 
 export default connect(({ mapView }) => ({
-  currentColormap:mapView.currentColormap,
-  mapView
+  currentColormap: mapView.currentColormap,
+  mapView,
 }))(Legend);
 
