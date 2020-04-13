3a802afb020bbc23286d8ac9db17cc7c09b9a8a9
Administrator <xcbx>
2020-04-13 15:18:29 +0800

    图例可拖动

--- src/pages/OnlineMapping/components/MapLegend/index.js
+++ src/pages/OnlineMapping/components/MapLegend/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState} from 'react';
 import styles from './index.less';
 import { connect } from 'dva';
 
@@ -7,10 +7,47 @@ function MapLegend(props){
   const { style_bottom, legend_discrete, legend_continuous } = props;
   const display = (legend_discrete || legend_continuous)? 'block':'none';
   const Width = (legend_discrete && legend_continuous)? '140px':'85px';
+  
+  const [translateX, setX] = useState(0);
+  const [translateY, setY] = useState(0);
+  const [lastX, setLastX] = useState(null);
+  const [lastY, setLastY] = useState(null);
+  const [moving, setMoving] = useState(false);
+  const onMouseDown = e => {
+        //e.stopPropagation();
+        setMoving(true);		
+    }
+	
+  const onMouseUp = e => {
+        setMoving(false);
+		setLastX(null);
+		setLastY(null);
+    }	
+  
+  const onMouseMove = (e) => {
+		if(!moving) return;
+		console.log('MouseMove moving:'+moving);
+		console.log()
+		if(lastX && lastY) {
+            let dx = e.clientX - lastX;
+            let dy = e.clientY - lastY;
+            setX(translateX + dx);
+			setY(translateY + dy);
+        }
+        setLastX (e.clientX);
+        setLastY (e.clientY);
+  }
+  const strTransform ='translate('+translateX+'px,'+translateY+'px)';	
+  
   return(	  
 			<div id="legend" 
-				style={{width:Width, display:display, bottom:style_bottom}} 
-				className={styles.legend} 				
+				style={{width:Width, display:display, bottom:style_bottom,
+						transform:strTransform
+						}} 
+				className={styles.legend} 			
+				onMouseDown={onMouseDown}	
+				onMouseUp={onMouseUp}
+				onMouseMove={onMouseMove}
 			>
 			    <h4 align="center">Legend</h4>		
 				{legend_continuous &&
